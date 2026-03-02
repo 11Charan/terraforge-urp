02f5a961ca845a373f8854beb1a689d2197b69b6
Sricharan Sureshkumar <sricharan@Mac.fritz.box>
2026-03-02 08:43:43 +0100

    feat: burst and parallel job

--- Assets/Scripts/TerrainChunk.cs
+++ Assets/Scripts/TerrainChunk.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
-using System.Threading;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Burst;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
@@ -10,55 +13,63 @@ public class TerrainChunk : MonoBehaviour
     public float noiseScale = 0.05f;
     public float heightMultiplier = 5f;
     public Material material;
-    private Mesh data;
-    private bool meshReady = false;
-    private MeshData meshData;
 
     public void Initialize(Vector2 offset)
     {
-        RequestMeshData(offset);
+        GenerateMesh(offset);
     }
 
-    MeshData GenerateMeshData(Vector2 offset)
+    void GenerateMesh(Vector2 offset)
     {
         int vertexCount = (gridSize + 1) * (gridSize + 1);
-        Vector3[] vertices = new Vector3[vertexCount];
-        Vector2[] uv = new Vector2[vertexCount];
-        Vector3[] normals = new Vector3[vertexCount];
-        int[] triangles = new int[gridSize * gridSize * 6];
+        int triangleCount = gridSize * gridSize * 6;
 
-        MeshData data = new(vertexCount, gridSize * gridSize * 6);
+        NativeArray<float3> vertices = new NativeArray<float3>(vertexCount, Allocator.TempJob);
+        NativeArray<float3> normals = new NativeArray<float3>(vertexCount, Allocator.TempJob);
 
-        int vertexIndex = 0;
-
-        for (int z = 0; z <= gridSize; z++)
+        TerrainJob job = new TerrainJob
         {
-            for (int x = 0; x <= gridSize; x++)
-            {
-                float worldX = x + offset.x;
-                float worldZ = z + offset.y;
+            gridSize = gridSize,
+            cellSize = cellSize,
+            noiseScale = noiseScale,
+            heightMultiplier = heightMultiplier,
+            offset = new float2(offset.x, offset.y),
+            vertices = vertices,
+            normals = normals
+        };
 
-                float height = Mathf.PerlinNoise(worldX * noiseScale, worldZ * noiseScale) * heightMultiplier;
+        JobHandle handle = job.Schedule(vertexCount, 64);
+        handle.Complete();
 
-                vertices[vertexIndex] = new Vector3(x * cellSize, height, z * cellSize);
+        // Convert to Unity arrays
+        Vector3[] finalVertices = new Vector3[vertexCount];
+        Vector3[] finalNormals = new Vector3[vertexCount];
 
-                float heightL = Mathf.PerlinNoise((worldX - 1) * noiseScale, worldZ * noiseScale) * heightMultiplier;
-                float heightR = Mathf.PerlinNoise((worldX + 1) * noiseScale, worldZ * noiseScale) * heightMultiplier;
-                float heightD = Mathf.PerlinNoise(worldX * noiseScale, (worldZ - 1) * noiseScale) * heightMultiplier;
-                float heightU = Mathf.PerlinNoise(worldX * noiseScale, (worldZ + 1) * noiseScale) * heightMultiplier;
-
-                Vector3 normal = new Vector3(
-                    heightL - heightR,
-                    2f,
-                    heightD - heightU
-                ).normalized;
-
-                normals[vertexIndex] = normal;
-
-                uv[vertexIndex] = new Vector2((float)x / gridSize, (float)z / gridSize);
-                vertexIndex++;
-            }
+        for (int i = 0; i < vertexCount; i++)
+        {
+            finalVertices[i] = vertices[i];
+            finalNormals[i] = normals[i];
         }
+
+        vertices.Dispose();
+        normals.Dispose();
+
+        int[] triangles = GenerateTriangles();
+
+        Mesh mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+
+        mesh.vertices = finalVertices;
+        mesh.triangles = triangles;
+        mesh.normals = finalNormals;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().material = material;
+    }
+
+    int[] GenerateTriangles()
+    {
+        int[] triangles = new int[gridSize * gridSize * 6];
 
         int triangleIndex = 0;
         int vert = 0;
@@ -81,49 +92,6 @@ public class TerrainChunk : MonoBehaviour
             vert++;
         }
 
-        data.vertices = vertices;
-        data.triangles = triangles;
-        data.uv = uv;
-        data.normals = normals;
-
-        return data;
-    }
-
-    void RequestMeshData(Vector2 offset)
-    {
-        Thread thread = new Thread(() =>
-        {
-            meshData = GenerateMeshData(offset);
-            meshReady = true;
-        });
-
-        thread.Start();
-    }
-
-    void Update()
-    {
-        if (meshReady)
-        {
-            ApplyMeshData();
-            meshReady = false;
-        }
-    }
-
-    void ApplyMeshData()
-    {
-        Mesh mesh = new Mesh
-        {
-            indexFormat = UnityEngine.Rendering.IndexFormat.UInt32,
-
-            vertices = meshData.vertices,
-            triangles = meshData.triangles,
-            uv = meshData.uv,
-            normals = meshData.normals
-        };
-
-        GetComponent<MeshFilter>().mesh = mesh;
-
-        MeshRenderer renderer = GetComponent<MeshRenderer>();
-        renderer.material = material;
+        return triangles;
     }
 }
